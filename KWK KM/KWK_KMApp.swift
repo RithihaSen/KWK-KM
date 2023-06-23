af2dc52c19297548b2820189a15738b8269253a3
Rithu <rithihasen@gmail.com>
2023-06-23 10:56:15 -0400

    Initial Commit

---
+++ KWK KM/KWK_KMApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_KMApp.swift
+//  KWK KM
+//
+//  Created by Senthilkumar Ethirajulu on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_KMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
